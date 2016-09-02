6c51e95bec1ae5baf2c56f226d7d12d62d0794fb
mvlabat <mvlabat@gmail.com>
2016-09-03 01:24:31 +0300

    Add initial plugin structure

---
+++ Source/ForestGenerator/Private/ForestGeneratorModule.cpp
@@ -0,0 +1,27 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "ForestGeneratorPrivatePCH.h"
+#include "EditorStyle.h"
+#include "EditorModeRegistry.h"
+
+#define LOCTEXT_NAMESPACE "FForestGeneratorModule"
+
+void FForestGeneratorModule::StartupModule()
+{
+	FEditorModeRegistry::Get().RegisterMode<FEdModeForest>(
+		TEXT("EM_Forest"),
+		NSLOCTEXT("EditorModes", "ForestMode", "Forest"),
+		FSlateIcon(FEditorStyle::GetStyleSetName(), "LevelEditor.FoliageMode", "LevelEditor.FoliageMode.Small"),
+		true, 400
+		);
+}
+
+void FForestGeneratorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FForestGeneratorModule, ForestGenerator)
